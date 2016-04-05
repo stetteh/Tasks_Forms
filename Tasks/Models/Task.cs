67af7bb5caac2a75a91998921a172f05d92dc837
stetteh <stettehq@gmail.com>
2016-04-05 13:33:01 -0500

    model created

---
+++ Tasks/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tasks.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Isdone { get; set; }
+
+        public Task(int id, string name, string desc, bool isdone)
+        {
+            Id = id;
+            Name = name;
+            Description = desc;
+            Isdone = isdone;
+        }
+    }
+}
