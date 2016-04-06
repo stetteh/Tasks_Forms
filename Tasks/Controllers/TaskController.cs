e92cad98b065736d568417f8bac5eb797facba18
stetteh <stettehq@gmail.com>
2016-04-05 19:12:10 -0500

    controller and views

--- Tasks/Controllers/TaskController.cs
+++ Tasks/Controllers/TaskController.cs
@@ -12,21 +12,44 @@ namespace Tasks.Controllers
         // GET: Task
         public ActionResult Index()
         {
-            List<Task> task = new List<Task>()
-            {
-                new Task(1, "Seth Quaye", "Go Home", false),
-                new Task(2, "Seth Quaye", "Eat Lunch", true),
-            };
+            List<Task> currentTasks;
+            currentTasks = (List<Task>)Session["tasks"] ?? new List<Task>();
+            //List<Task> task = new List<Task>()
+            //{
+            //    new Task(1, "Seth Quaye", "Go Home", false),
+            //    new Task(2, "Seth Quaye", "Eat Lunch", true),
+            //};
 
-            return View(task);
+            return View(currentTasks);
+        }
+
+        //Post Task
+        [HttpGet]
+        public ActionResult Create()
+        { 
+            return View();
         }
 
         //Post Task
         [HttpPost]
-        public ActionResult Create()
+        public ActionResult Create(Task newTask)
         {
+            if (ModelState.IsValid)
+            {
+                var currentTasks = (List<Task>)Session["tasks"];
+                if (currentTasks == null)
+                {
+                    currentTasks = new List<Task>();
+                }
+                currentTasks.Add(newTask);
 
-            return Index();
+                Session["tasks"] = currentTasks;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- Tasks/Models/Task.cs
+++ Tasks/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace Tasks.Models
     public class Task
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public bool Isdone { get; set; }
 
+        public Task()
+        {
+            
+        }
         public Task(int id, string name, string desc, bool isdone)
         {
             Id = id;
